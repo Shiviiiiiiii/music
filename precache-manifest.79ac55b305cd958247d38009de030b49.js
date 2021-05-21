e6529bf2ed8ff8e650801ec15350505dab8bd95b
Deleplace Lucas <ldeleplace@ikomibi.com>
2021-05-21 14:08:14 +0200

    Updates

---
+++ precache-manifest.79ac55b305cd958247d38009de030b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ecc53fd4235097a3efb5842e23292a8",
+    "url": "/mudic/index.html"
+  },
+  {
+    "revision": "c7e234d6e23e16534b3f",
+    "url": "/mudic/static/css/main.b554a20d.chunk.css"
+  },
+  {
+    "revision": "806fe4eee2f235ecce87",
+    "url": "/mudic/static/js/2.8e1019eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mudic/static/js/2.8e1019eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e234d6e23e16534b3f",
+    "url": "/mudic/static/js/main.86fc5c21.chunk.js"
+  },
+  {
+    "revision": "f04ea4a6d6b88a339658",
+    "url": "/mudic/static/js/runtime-main.bbd7eee3.js"
+  }
+]);
