10a345fdc40ca6201f39d72417664a777819f88c
Deleplace Lucas <ldeleplace@ikomibi.com>
2021-05-21 13:41:56 +0200

    initial commit

---
+++ src/context/songs.js
@@ -0,0 +1,26 @@
+export const songsArr = [
+  [
+    "Eminem - Without me",
+    "https://www.cjoint.com/doc/21_05/KEvjrG0Slri_eminem-without-me-official-music-video.mp3"
+  ],
+  [
+    "Sir Skye - Amour impossible",
+    "https://www.cjoint.com/doc/21_05/KEvj6A2jnai_sir-skye-amour-impossible-official-audio.mp3"
+  ],
+  [
+    "My Darkest Days ft. Ludacris, Zakk Wylde - Porn Star Dancing",
+    "https://www.cjoint.com/doc/21_05/KEvj64wwLsi_my-darkest-days-ft-ludacris-zakk-wylde-porn-star-dancing-official-extendeduncensored-version.mp3"
+  ],
+  [
+    "2KBABY x Marshmello - Like This",
+    "https://www.cjoint.com/doc/21_05/KEvkbXLxJ5i_2kbaby-x-marshmello-like-this-official-music-video.mp3"
+  ],
+  [
+    "Coolio - Gangsta's Paradise - feat. L.V.",
+    "https://www.cjoint.com/doc/21_05/KEvkdW7fy7i_coolio-gangstas-paradise-feat-lv-official-music-video.mp3"
+  ],
+  [
+    "Moji x Sboy ft. Luv Resval - CHIMIQUE",
+    "https://www.cjoint.com/doc/21_05/KEvlItqFUXi_moji-x-sboy-ft-luv-resval-chimique.mp3"
+  ]
+];
